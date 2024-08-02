b7b0cc9c5acd79a73cc137d1f976752fdbe86353
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-08-02 23:26:24 +0300

    Create User model

---
+++ src/main/java/com/taskmanagementsystem/model/User.java
@@ -0,0 +1,40 @@
+package com.taskmanagementsystem.model;
+
+import com.taskmanagementsystem.model.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
